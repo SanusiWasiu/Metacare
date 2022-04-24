d118be0443978c1e8adcf9fb0c17c5a7f66ad014
SanusiWasiu <was.sanusi@gmail.com>
2022-04-24 16:26:49 +0100

    update all

--- src/assets/styles/TabWrapper.styled.js
+++ src/assets/styles/TabWrapper.styled.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
 export const TabWrapper = styled.div`
-    margin-top: 10rem;
+    margin-top: 2rem;
+    font-family: 'Gelion';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    margin-left: 2rem;
 `;
--- src/components/main-body-components/Tabs.js
+++ src/components/main-body-components/Tabs.js
@@ -4,7 +4,7 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import TabWrapper from '../../assets/styles/TabWrapper.styled'
+import { TabWrapper } from '../../assets/styles/TabWrapper.styled'
 
 export const Tabs = () => {
   const [value, setValue] = React.useState('1');
